3e5577c9c81dfeb7334dd3b8665e0ea3cc0ac820
krnjin <krnjin94@gmail.com>
2019-07-15 15:20:02 -0700

    added iife

--- Modules/app.js
+++ Modules/app.js
@@ -4,6 +4,13 @@
 var greet = require('./greet');
 var {firstFunction, secondFunction} = require('./moduleTest');
 
+
+(function (example, number){
+    console.log("this is an example of immediate invoked function");
+    console.log(example);
+    console.log(number);
+}("something", 5));
+
 // invoking multiple variables from one module
 greet.greet();
 greet.something();
